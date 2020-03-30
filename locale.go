3e0d49ecf6132d117b40d36a6c67bdc9083ace15
matszpk <matszpk@interia.pl>
2020-03-30 20:33:30 +0200

    Add locale formatting.

---
+++ locale.go
@@ -0,0 +1,151 @@
+/*
+ * locale.go - locale routines
+ *
+ * goint128 - go int128 library
+ * Copyright (C) 2019  Mateusz Szpakowski
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+// Package to operate 128-bit integer
+package goint128
+
+type locFmt struct {
+    comma, sep1000 rune
+    sep100and1000 bool
+    digits []rune
+}
+
+var normalDigits []rune = []rune("0123456789")
+var arDigits []rune = []rune("٠١٢٣٤٥٦٧٨٩")
+var faDigits []rune = []rune("۰۱۲۳۴۵۶۷۸۹")
+var bnDigits []rune = []rune("০১২৩৪৫৬৭৮৯")
+var mrDigits []rune = []rune("०१२३४५६७८९")
+var myDigits []rune = []rune("၀၁၂၃၄၅၆၇၈၉")
+
+var defaultLocaleFormat locFmt = locFmt{ '.', ',', false, normalDigits }
+
+var localeFormats map[string]locFmt = map[string]locFmt {
+    "af": locFmt{ ',', ' ', false, normalDigits },
+    "am": locFmt{ '.', ',', false, normalDigits },
+    "ar": locFmt{ '٫', '٬', false, arDigits },
+    "az": locFmt{ ',', '.', false, normalDigits },
+    "bg": locFmt{ ',', ' ', false, normalDigits },
+    "bn": locFmt{ '.', ',', true, bnDigits },
+    "ca": locFmt{ ',', '.', false, normalDigits },
+    "cs": locFmt{ ',', ' ', false, normalDigits },
+    "da": locFmt{ ',', '.', false, normalDigits },
+    "de": locFmt{ ',', '.', false, normalDigits },
+    "el": locFmt{ ',', '.', false, normalDigits },
+    "en": locFmt{ '.', ',', false, normalDigits },
+    "es": locFmt{ ',', '.', false, normalDigits },
+    "et": locFmt{ ',', ' ', false, normalDigits },
+    "fa": locFmt{ '٫', '٬', false, faDigits },
+    "fi": locFmt{ ',', ' ', false, normalDigits },
+    "fil": locFmt{ '.', ',', false, normalDigits },
+    "fr": locFmt{ ',', ' ', false, normalDigits },
+    "gu": locFmt{ '.', ',', true, normalDigits },
+    "he": locFmt{ '.', ',', false, normalDigits },
+    "hi": locFmt{ '.', ',', true, normalDigits },
+    "hr": locFmt{ ',', '.', false, normalDigits },
+    "hu": locFmt{ ',', ' ', false, normalDigits },
+    "hy": locFmt{ ',', ' ', false, normalDigits },
+    "id": locFmt{ ',', '.', false, normalDigits },
+    "is": locFmt{ ',', '.', false, normalDigits },
+    "it": locFmt{ ',', '.', false, normalDigits },
+    "ja": locFmt{ '.', ',', false, normalDigits },
+    "ka": locFmt{ ',', ' ', false, normalDigits },
+    "kk": locFmt{ ',', ' ', false, normalDigits },
+    "km": locFmt{ ',', '.', false, normalDigits },
+    "kn": locFmt{ '.', ',', false, normalDigits },
+    "ko": locFmt{ '.', ',', false, normalDigits },
+    "ky": locFmt{ ',', ' ', false, normalDigits },
+    "lo": locFmt{ ',', '.', false, normalDigits },
+    "lt": locFmt{ ',', ' ', false, normalDigits },
+    "lv": locFmt{ ',', ' ', false, normalDigits },
+    "mk": locFmt{ ',', '.', false, normalDigits },
+    "ml": locFmt{ '.', ',', true, normalDigits },
+    "mn": locFmt{ '.', ',', false, normalDigits },
+    "mo": locFmt{ ',', '.', false, normalDigits },
+    "mr": locFmt{ '.', ',', true, mrDigits },
+    "ms": locFmt{ '.', ',', false, normalDigits },
+    "mul": locFmt{ '.', ',', false, normalDigits },
+    "my": locFmt{ '.', ',', false, myDigits },
+    "nb": locFmt{ ',', ' ', false, normalDigits },
+    "ne": locFmt{ '.', ',', false, mrDigits },
+    "nl": locFmt{ ',', '.', false, normalDigits },
+    "no": locFmt{ '.', ',', false, normalDigits },
+    "pa": locFmt{ '.', ',', true, normalDigits },
+    "pl": locFmt{ ',', ' ', false, normalDigits },
+    "pt": locFmt{ ',', '.', false, normalDigits },
+    "ro": locFmt{ ',', '.', false, normalDigits },
+    "ru": locFmt{ ',', ' ', false, normalDigits },
+    "sh": locFmt{ ',', '.', false, normalDigits },
+    "si": locFmt{ '.', ',', false, normalDigits },
+    "sk": locFmt{ ',', ' ', false, normalDigits },
+    "sl": locFmt{ ',', '.', false, normalDigits },
+    "sq": locFmt{ ',', ' ', false, normalDigits },
+    "sr": locFmt{ ',', '.', false, normalDigits },
+    "sv": locFmt{ ',', ' ', false, normalDigits },
+    "sw": locFmt{ '.', ',', false, normalDigits },
+    "ta": locFmt{ '.', ',', true, normalDigits },
+    "te": locFmt{ '.', ',', false, normalDigits },
+    "th": locFmt{ '.', ',', false, normalDigits },
+    "tl": locFmt{ '.', ',', false, normalDigits },
+    "tn": locFmt{ '.', ',', false, normalDigits },
+    "tr": locFmt{ ',', '.', false, normalDigits },
+    "uk": locFmt{ ',', ' ', false, normalDigits },
+    "ur": locFmt{ '.', ',', false, normalDigits },
+    "uz": locFmt{ ',', ' ', false, normalDigits },
+    "vi": locFmt{ ',', '.', false, normalDigits },
+    "zh": locFmt{ '.', ',', false, normalDigits },
+    "zu": locFmt{ '.', ',', false, normalDigits },
+}
+
+func (a UInt128) Locale(lang string) string {
+    l, ok := localeFormats[lang]
+    if !ok { l = defaultLocaleFormat }
+    
+    s := a.Format()
+    os := make([]rune, 0, len(s))
+    slen := len(s)
+    ti := slen
+    i := slen
+    if !l.sep100and1000 {
+        ti = (slen)%3
+        if ti==0 { ti=3 }
+    }
+    for _, r := range s {
+        if r>='0' || r<='9' {
+            os = append(os, l.digits[r-'0'])
+        }
+        if i!=1 {
+            if !l.sep100and1000 || ti<=3 {
+                ti--
+                if ti==0 {
+                    os = append(os, l.sep1000)
+                    ti = 3
+                }
+            } else {
+                ti--
+                if (ti-3)&1==0 {
+                    os = append(os, l.sep1000)
+                }
+            }
+        }
+        i--
+    }
+    return string(os)
+}
---
+++ locale_test.go
@@ -0,0 +1,132 @@
+/*
+ * locale_test.go - tests for int128 routines
+ *
+ * goint128 - go int128 library
+ * Copyright (C) 2019  Mateusz Szpakowski
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package goint128
+
+import (
+    "testing"
+)
+
+type UInt128LocTC struct {
+    lang string
+    a UInt128
+    expected string
+}
+
+func TestUInt128Locale(t *testing.T) {
+    testCases := []UInt128LocTC {
+        UInt128LocTC{ "af", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "am", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "ar", UInt128{1234567890,0}, "١٬٢٣٤٬٥٦٧٬٨٩٠" },
+        UInt128LocTC{ "az", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "bg", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "bn", UInt128{1234567890,0}, "১,২৩,৪৫,৬৭,৮৯০", },
+        UInt128LocTC{ "ca", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "cs", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "da", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "de", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "el", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "en", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "es", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "et", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "fa", UInt128{1234567890,0}, "۱٬۲۳۴٬۵۶۷٬۸۹۰" },
+        UInt128LocTC{ "fi", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "fil", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "fr", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "gu", UInt128{1234567890,0}, "1,23,45,67,890" },
+        UInt128LocTC{ "he", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "hi", UInt128{1234567890,0}, "1,23,45,67,890" },
+        UInt128LocTC{ "hr", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "hu", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "hy", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "id", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "is", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "it", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "ja", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "ka", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "kk", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "km", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "kn", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "ko", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "ky", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "lo", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "lt", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "lv", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "mk", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "ml", UInt128{1234567890,0}, "1,23,45,67,890" },
+        UInt128LocTC{ "mn", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "mo", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "mr", UInt128{1234567890,0}, "१,२३,४५,६७,८९०" },
+        UInt128LocTC{ "ms", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "mul", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "my", UInt128{1234567890,0}, "၁,၂၃၄,၅၆၇,၈၉၀" },
+        UInt128LocTC{ "nb", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "ne", UInt128{1234567890,0}, "१,२३४,५६७,८९०" },
+        UInt128LocTC{ "nl", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "no", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "pa", UInt128{1234567890,0}, "1,23,45,67,890" },
+        UInt128LocTC{ "pl", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "pt", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "ro", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "ru", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "sh", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "si", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "sk", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "sl", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "sq", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "sr", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "sv", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "sw", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "ta", UInt128{1234567890,0}, "1,23,45,67,890" },
+        UInt128LocTC{ "te", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "th", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "tl", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "tn", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "tr", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "uk", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "ur", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "uz", UInt128{1234567890,0}, "1 234 567 890" },
+        UInt128LocTC{ "vi", UInt128{1234567890,0}, "1.234.567.890" },
+        UInt128LocTC{ "zh", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "zu", UInt128{1234567890,0}, "1,234,567,890" },
+        UInt128LocTC{ "en", UInt128{123456789,0}, "123,456,789" },
+        UInt128LocTC{ "en", UInt128{23456789,0}, "23,456,789" },
+        UInt128LocTC{ "en", UInt128{789,0}, "789" },
+        UInt128LocTC{ "en", UInt128{89,0}, "89" },
+        UInt128LocTC{ "ta", UInt128{234567890,0}, "23,45,67,890" },
+        UInt128LocTC{ "ta", UInt128{34567890,0}, "3,45,67,890" },
+        UInt128LocTC{ "ta", UInt128{4567890,0}, "45,67,890" },
+        UInt128LocTC{ "ta", UInt128{890,0}, "890" },
+        UInt128LocTC{ "ta", UInt128{89,0}, "89" },
+        UInt128LocTC{ "ta", UInt128{9,0}, "9" },
+    }
+    for i, tc := range testCases {
+        a := tc.a
+        result := tc.a.Locale(tc.lang)
+        if tc.expected!=result {
+            t.Errorf("Result mismatch: %d: fmt(%v,%s)->%v!=%v",
+                     i, tc.a, tc.lang, tc.expected, result)
+        }
+        if tc.a!=a {
+            t.Errorf("Argument has been modified: %d %s: %v!=%v", i, tc.lang, a, tc.a)
+        }
+    }
+}
