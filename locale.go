ba2330eab94f129ee3f41a2cf889f50313847fc0
matszpk <matszpk@interia.pl>
2020-03-31 08:06:36 +0200

    Add alternate sep1000 and option to skip separator for thousands.

--- locale.go
+++ locale.go
@@ -26,7 +26,7 @@ import "strconv"
 
 // locale formatting info
 type LocFmt struct {
-    Comma, Sep1000 rune
+    Comma, Sep1000, Sep1000_2 rune
     Sep100and1000 bool
     Digits []rune
 }
@@ -38,83 +38,83 @@ var bnDigits []rune = []rune("০১২৩৪৫৬৭৮৯")
 var mrDigits []rune = []rune("०१२३४५६७८९")
 var myDigits []rune = []rune("၀၁၂၃၄၅၆၇၈၉")
 
-var defaultLocaleFormat LocFmt = LocFmt{ '.', ',', false, normalDigits }
+var defaultLocaleFormat LocFmt = LocFmt{ '.', ',', ',', false, normalDigits }
 
 var localeFormats map[string]LocFmt = map[string]LocFmt {
-    "af": LocFmt{ ',', ' ', false, normalDigits },
-    "am": LocFmt{ '.', ',', false, normalDigits },
-    "ar": LocFmt{ '٫', '٬', false, arDigits },
-    "az": LocFmt{ ',', '.', false, normalDigits },
-    "bg": LocFmt{ ',', ' ', false, normalDigits },
-    "bn": LocFmt{ '.', ',', true, bnDigits },
-    "ca": LocFmt{ ',', '.', false, normalDigits },
-    "cs": LocFmt{ ',', ' ', false, normalDigits },
-    "da": LocFmt{ ',', '.', false, normalDigits },
-    "de": LocFmt{ ',', '.', false, normalDigits },
-    "el": LocFmt{ ',', '.', false, normalDigits },
-    "en": LocFmt{ '.', ',', false, normalDigits },
-    "es": LocFmt{ ',', '.', false, normalDigits },
-    "et": LocFmt{ ',', ' ', false, normalDigits },
-    "fa": LocFmt{ '٫', '٬', false, faDigits },
-    "fi": LocFmt{ ',', ' ', false, normalDigits },
-    "fil": LocFmt{ '.', ',', false, normalDigits },
-    "fr": LocFmt{ ',', ' ', false, normalDigits },
-    "gu": LocFmt{ '.', ',', true, normalDigits },
-    "he": LocFmt{ '.', ',', false, normalDigits },
-    "hi": LocFmt{ '.', ',', true, normalDigits },
-    "hr": LocFmt{ ',', '.', false, normalDigits },
-    "hu": LocFmt{ ',', ' ', false, normalDigits },
-    "hy": LocFmt{ ',', ' ', false, normalDigits },
-    "id": LocFmt{ ',', '.', false, normalDigits },
-    "is": LocFmt{ ',', '.', false, normalDigits },
-    "it": LocFmt{ ',', '.', false, normalDigits },
-    "ja": LocFmt{ '.', ',', false, normalDigits },
-    "ka": LocFmt{ ',', ' ', false, normalDigits },
-    "kk": LocFmt{ ',', ' ', false, normalDigits },
-    "km": LocFmt{ ',', '.', false, normalDigits },
-    "kn": LocFmt{ '.', ',', false, normalDigits },
-    "ko": LocFmt{ '.', ',', false, normalDigits },
-    "ky": LocFmt{ ',', ' ', false, normalDigits },
-    "lo": LocFmt{ ',', '.', false, normalDigits },
-    "lt": LocFmt{ ',', ' ', false, normalDigits },
-    "lv": LocFmt{ ',', ' ', false, normalDigits },
-    "mk": LocFmt{ ',', '.', false, normalDigits },
-    "ml": LocFmt{ '.', ',', true, normalDigits },
-    "mn": LocFmt{ '.', ',', false, normalDigits },
-    "mo": LocFmt{ ',', '.', false, normalDigits },
-    "mr": LocFmt{ '.', ',', true, mrDigits },
-    "ms": LocFmt{ '.', ',', false, normalDigits },
-    "mul": LocFmt{ '.', ',', false, normalDigits },
-    "my": LocFmt{ '.', ',', false, myDigits },
-    "nb": LocFmt{ ',', ' ', false, normalDigits },
-    "ne": LocFmt{ '.', ',', false, mrDigits },
-    "nl": LocFmt{ ',', '.', false, normalDigits },
-    "no": LocFmt{ '.', ',', false, normalDigits },
-    "pa": LocFmt{ '.', ',', true, normalDigits },
-    "pl": LocFmt{ ',', ' ', false, normalDigits },
-    "pt": LocFmt{ ',', '.', false, normalDigits },
-    "ro": LocFmt{ ',', '.', false, normalDigits },
-    "ru": LocFmt{ ',', ' ', false, normalDigits },
-    "sh": LocFmt{ ',', '.', false, normalDigits },
-    "si": LocFmt{ '.', ',', false, normalDigits },
-    "sk": LocFmt{ ',', ' ', false, normalDigits },
-    "sl": LocFmt{ ',', '.', false, normalDigits },
-    "sq": LocFmt{ ',', ' ', false, normalDigits },
-    "sr": LocFmt{ ',', '.', false, normalDigits },
-    "sv": LocFmt{ ',', ' ', false, normalDigits },
-    "sw": LocFmt{ '.', ',', false, normalDigits },
-    "ta": LocFmt{ '.', ',', true, normalDigits },
-    "te": LocFmt{ '.', ',', false, normalDigits },
-    "th": LocFmt{ '.', ',', false, normalDigits },
-    "tl": LocFmt{ '.', ',', false, normalDigits },
-    "tn": LocFmt{ '.', ',', false, normalDigits },
-    "tr": LocFmt{ ',', '.', false, normalDigits },
-    "uk": LocFmt{ ',', ' ', false, normalDigits },
-    "ur": LocFmt{ '.', ',', false, normalDigits },
-    "uz": LocFmt{ ',', ' ', false, normalDigits },
-    "vi": LocFmt{ ',', '.', false, normalDigits },
-    "zh": LocFmt{ '.', ',', false, normalDigits },
-    "zu": LocFmt{ '.', ',', false, normalDigits },
+    "af": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "am": LocFmt{ '.', ',', ',', false, normalDigits },
+    "ar": LocFmt{ '٫', '٬', '٬', false, arDigits },
+    "az": LocFmt{ ',', '.', '.', false, normalDigits },
+    "bg": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "bn": LocFmt{ '.', ',', ',', true, bnDigits },
+    "ca": LocFmt{ ',', '.', '.',false, normalDigits },
+    "cs": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "da": LocFmt{ ',', '.', '.', false, normalDigits },
+    "de": LocFmt{ ',', '.', '.', false, normalDigits },
+    "el": LocFmt{ ',', '.', '.', false, normalDigits },
+    "en": LocFmt{ '.', ',', ',', false, normalDigits },
+    "es": LocFmt{ ',', '.', '.', false, normalDigits },
+    "et": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "fa": LocFmt{ '٫', '٬', '٬', false, faDigits },
+    "fi": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "fil": LocFmt{ '.', ',', ',', false, normalDigits },
+    "fr": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "gu": LocFmt{ '.', ',', ',', true, normalDigits },
+    "he": LocFmt{ '.', ',', ',', false, normalDigits },
+    "hi": LocFmt{ '.', ',', ',', true, normalDigits },
+    "hr": LocFmt{ ',', '.', '.', false, normalDigits },
+    "hu": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "hy": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "id": LocFmt{ ',', '.', '.', false, normalDigits },
+    "is": LocFmt{ ',', '.', '.', false, normalDigits },
+    "it": LocFmt{ ',', '.', '.', false, normalDigits },
+    "ja": LocFmt{ '.', ',', ',', false, normalDigits },
+    "ka": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "kk": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "km": LocFmt{ ',', '.', '.', false, normalDigits },
+    "kn": LocFmt{ '.', ',', ',', false, normalDigits },
+    "ko": LocFmt{ '.', ',', ',', false, normalDigits },
+    "ky": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "lo": LocFmt{ ',', '.', '.', false, normalDigits },
+    "lt": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "lv": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "mk": LocFmt{ ',', '.', '.', false, normalDigits },
+    "ml": LocFmt{ '.', ',', ',', true, normalDigits },
+    "mn": LocFmt{ '.', ',', ',', false, normalDigits },
+    "mo": LocFmt{ ',', '.', '.', false, normalDigits },
+    "mr": LocFmt{ '.', ',', ',', true, mrDigits },
+    "ms": LocFmt{ '.', ',', ',', false, normalDigits },
+    "mul": LocFmt{ '.', ',', ',', false, normalDigits },
+    "my": LocFmt{ '.', ',', ',', false, myDigits },
+    "nb": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "ne": LocFmt{ '.', ',', ',', false, mrDigits },
+    "nl": LocFmt{ ',', '.', '.', false, normalDigits },
+    "no": LocFmt{ '.', ',', ',', false, normalDigits },
+    "pa": LocFmt{ '.', ',', ',', true, normalDigits },
+    "pl": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "pt": LocFmt{ ',', '.', '.', false, normalDigits },
+    "ro": LocFmt{ ',', '.', '.', false, normalDigits },
+    "ru": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "sh": LocFmt{ ',', '.', '.', false, normalDigits },
+    "si": LocFmt{ '.', ',', ',', false, normalDigits },
+    "sk": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "sl": LocFmt{ ',', '.', '.', false, normalDigits },
+    "sq": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "sr": LocFmt{ ',', '.', '.', false, normalDigits },
+    "sv": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "sw": LocFmt{ '.', ',', ',', false, normalDigits },
+    "ta": LocFmt{ '.', ',', ',', true, normalDigits },
+    "te": LocFmt{ '.', ',', ',', false, normalDigits },
+    "th": LocFmt{ '.', ',', ',', false, normalDigits },
+    "tl": LocFmt{ '.', ',', ',', false, normalDigits },
+    "tn": LocFmt{ '.', ',', ',', false, normalDigits },
+    "tr": LocFmt{ ',', '.', '.', false, normalDigits },
+    "uk": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "ur": LocFmt{ '.', ',', ',', false, normalDigits },
+    "uz": LocFmt{ ',', ' ', ' ', false, normalDigits },
+    "vi": LocFmt{ ',', '.', '.', false, normalDigits },
+    "zh": LocFmt{ '.', ',', ',', false, normalDigits },
+    "zu": LocFmt{ '.', ',', ',', false, normalDigits },
 }
 
 // get locale formating info
@@ -132,7 +132,7 @@ func GetLocFmt(lang string) *LocFmt {
 }
 
 // format 128-bit unsigned integer including locale
-func (a UInt128) LocaleFormat(lang string) string {
+func (a UInt128) LocaleFormat(lang string, sep1000 bool) string {
     l := GetLocFmt(lang)
     s := a.Format()
     os := make([]rune, 0, len(s))
@@ -147,7 +147,7 @@ func (a UInt128) LocaleFormat(lang string) string {
         if r>='0' && r<='9' {
             os = append(os, l.Digits[r-'0'])
         }
-        if i!=1 {
+        if sep1000 && i!=1 {
             if !l.Sep100and1000 || ti<=3 {
                 ti--
                 if ti==0 {
@@ -176,7 +176,7 @@ func LocaleParseUInt128(lang, str string) (UInt128, error) {
         if r>='0' && r<='9' {
             // if standard digits
             os = append(os, r)
-        } else if r!=l.Sep1000 {
+        } else if r!=l.Sep1000 && r!=l.Sep1000_2 {
             // if non-standard digit
             dig:=0
             found := false
